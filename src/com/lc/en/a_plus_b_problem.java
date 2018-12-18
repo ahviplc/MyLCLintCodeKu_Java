f49154c66eb37bc0594d437d1b72126390ee59d2
LC <ahlc@sina.cn>
2018-12-18 11:01:28 +0800

    Initial commit

---
+++ src/com/lc/en/a_plus_b_problem.java
@@ -0,0 +1,29 @@
+package com.lc.en;
+
+public class a_plus_b_problem {
+
+    // java
+
+    /*
+     * @param : An integer
+     * @param : An integer
+     * @return: The sum of a and b
+     */
+    public static int aplusb(int a, int b) {
+
+        int sum_without_carry, carry;
+
+        sum_without_carry = a ^ b; //没有进位的和
+        carry = (a & b) << 1; //进位
+        if (carry == 0)
+            return sum_without_carry;
+        else
+            return aplusb(sum_without_carry, carry);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(aplusb(0, 1));
+    }
+
+}
