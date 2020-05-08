bb7c75a29cde339555c43445ca6ca1a75a86c7d2
ahviplc <ahlc@sina.cn>
2020-05-08 16:09:13 +0800

    add LintCode-zh 2. Add Two Numbers(两数相加)! LC 2020-5-8 16:09:03

---
+++ src/com/lc/zh/AddTwoNumbers_2.java
@@ -0,0 +1,71 @@
+package com.lc.zh;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * <p>
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * <p>
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/add-two-numbers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+
+
+class AddTwoNumbers_2 {
+
+    /**
+     * ListNode类
+     */
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode(-1);
+        ListNode cur = res;
+        int quotient = 0;
+        while (l1 != null || l2 != null || quotient != 0) {
+            int t = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + quotient;
+            quotient = t / 10;
+            ListNode node = new ListNode(t % 10);
+            cur.next = node;
+            cur = node;
+            l1 = (l1 == null) ? l1 : l1.next;
+            l2 = (l2 == null) ? l2 : l2.next;
+        }
+        return res.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode1 = new ListNode(2);
+        listNode1.next = new ListNode(4);
+        listNode1.next.next = new ListNode(3);
+        ListNode listNode2 = new ListNode(5);
+        listNode2.next = new ListNode(6);
+        listNode2.next.next = new ListNode(4);
+        ListNode listNodeTemp = addTwoNumbers(listNode1, listNode2);
+        System.out.println(String.valueOf(listNodeTemp.val) + String.valueOf(listNodeTemp.next.val) + String.valueOf(listNodeTemp.next.next.val));//708
+    }
+}
